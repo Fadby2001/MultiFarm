2f4e92ec0337de13e3cd1cf649ab83e600b41e8c
Silence <258494887+Fadby2001@users.noreply.github.com>
2026-08-07 22:04:48 -0400

    Simplify Loader by removing script key checks
    Removed key validation and script key writing.

--- Loader.lua
+++ Loader.lua
@@ -7,10 +7,4 @@ if not GamePlaceIDs[game.PlaceId] then
     game.Players.LocalPlayer:Kick("KagiHub | This game is not supported.")
     return
 end
-if not getfenv().script_key then
-    game.Players.LocalPlayer:Kick("KagiHub | Key not found, make sure to include the script_key part in the first line.")
-    return
-end
-writefile("KagiHubScriptKey", getfenv().script_key)
-
 loadstring(game:HttpGet(GamePlaceIDs[game.PlaceId]))()
